3306cacb586db0f6efd4ed38a62715c68da94de8
Tom Wang <s22659211@yahoo.com.tw>
2022-05-22 01:13:56 +0800

    add 其他

--- driving.py
+++ driving.py
@@ -6,8 +6,10 @@ if country == '台灣':
 		print('你可以考駕照')
 	else:
 		print('你不能考駕照')
-		if country == '美國':
+elif country == '美國':
 	if age >= 16:
 		print('你可以考駕照')
 	else:
 		print('你不能考駕照')
+else:
+	print('只能輸入台灣或美國')
